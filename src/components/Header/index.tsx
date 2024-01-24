142003c78521249ae857fe488f7f9478d88c0272
Sarang Gupta <saranggupta25@gmail.com>
2024-01-24 23:39:24 +0530

    Base Setup

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./header.scss";
+
+const Header = () => {
+  return (
+    <nav className="header">
+      <div>Logo</div>
+      <div>Navigation</div>
+    </nav>
+  );
+};
+
+export default Header;
